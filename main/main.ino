aa46fb7c20d4a84356d59ed71347309e7b559162
JHALE <jp@dhcp-rhodes-3200.redrover.cornell.edu>
2018-02-24 12:54:52 -0500

    added bunch of rat

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
+const int button = 2;       // connect a button
+const int buzzer = 4;       // connect a buzzer
+const int light = 3;
+const int funnylight = A0;
 
+#include "Grove_LED_Bar.h"
+Grove_LED_Bar bar(9, 8, true);
+
+void setup()
+{
+    pinMode(button, INPUT); //set button as an INPUT device
+    pinMode(buzzer, OUTPUT);   //set LED as an OUTPUT device
+    pinMode(light, OUTPUT);
+    pinMode(funnylight, OUTPUT);
+    
+
+   bar.begin();
 }
+void loop()
+{
+  //bar.setLevel(1);
+  /*
+   
+   
+    int btn = digitalRead(button); //read the status of the button
+    digitalWrite(buzzer, btn);
+    digitalWrite(light, btn);
+   // digitalWrite(funnylight, 0);
+    delay(10);
+    */
 
-void loop() {
-  // put your main code here, to run repeatedly:
-  std::cout << "Pls give me a free tshirt";
+    for(int i=0; i<=10; i++)
+    {
+//      bar.setLevel(i);
+      delay(100);
+    }
+    
 }
--- semperfi.cpp
+++ semperfi.cpp
@@ -8,5 +8,5 @@ void setup()
 void loop()
 {
   //put your main code here, to run repeated:
-  std::cout << "hhh";
+  std::cout << "hhh"
 }
