d9a64f30c22b468de12ffae0c38762fb2d5fd516
JHALE <jp@dhcp-rhodes-3200.redrover.cornell.edu>
2018-02-24 14:18:27 -0500

    skeletons for main classes

--- main/main.ino
+++ main/main.ino
@@ -3,12 +3,37 @@
 
 const size_t JSON_SIZE = 300;
 StaticJsonBuffer<JSON_SIZE> jsonBuffer;
-JsonObject& root;
+JsonObject& root = jsonBuffer.createObject();
+
+class FridgeGenie
+{
+public:
+  void run();
+private:
+  
+};
+
+class ArduinoInput
+{
+public:
+
+private:
+  
+};
+
+class ArduinoOutput
+{
+public:
+
+private:
+
+}
 
 void setup()
 {
   //StaticJsonBuffer<JSON_SIZE> jsonBuffer;
-  root = jsonBuffer.createObject();
+//  root = jsonBuffer.createObject();
+  
 }
 void loop()
 {
