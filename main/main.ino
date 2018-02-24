594b1abd59c4b22d079028cc794a93d421e111f6
JHALE <jp@dhcp-rhodes-3200.redrover.cornell.edu>
2018-02-24 14:08:03 -0500

    switched json library

--- main/main.ino
+++ main/main.ino
@@ -1,38 +1,16 @@
-const int button = 2;       // connect a button
-const int buzzer = 4;       // connect a buzzer
-const int light = 3;
-const int funnylight = A0;
+//#include <LiquidCrystal.h>
+#include "ArduinoJson.h"
 
-#include "Grove_LED_Bar.h"
-Grove_LED_Bar bar(9, 8, true);
+const size_t JSON_SIZE = 300;
+StaticJsonBuffer<JSON_SIZE> jsonBuffer;
+JsonObject& root;
 
 void setup()
 {
-    pinMode(button, INPUT); //set button as an INPUT device
-    pinMode(buzzer, OUTPUT);   //set LED as an OUTPUT device
-    pinMode(light, OUTPUT);
-    pinMode(funnylight, OUTPUT);
-    
-
-   bar.begin();
+  //StaticJsonBuffer<JSON_SIZE> jsonBuffer;
+  root = jsonBuffer.createObject();
 }
 void loop()
 {
-  //bar.setLevel(1);
-  /*
-   
-   
-    int btn = digitalRead(button); //read the status of the button
-    digitalWrite(buzzer, btn);
-    digitalWrite(light, btn);
-   // digitalWrite(funnylight, 0);
-    delay(10);
-    */
-
-    for(int i=0; i<=10; i++)
-    {
-//      bar.setLevel(i);
-      delay(100);
-    }
-    
+  
 }
