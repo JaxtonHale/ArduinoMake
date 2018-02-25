ca92863ae41ceb48ac9fbf85516623aa1a8f23ad
gamma2653 <Christopher2653@gmail.com>
2018-02-24 19:59:37 -0500

    Cool Maps bro

--- main/main.ino
+++ main/main.ino
@@ -2,12 +2,19 @@
 #include "ArduinoJson.h"
 #include "FridgeGenie.h"
 #include "pins.h"
-
+#include "HashMap.h"
 //const int MENU_LCD_PIN = 1
 
-
+const byte HASH_SIZE = 30;
 void setup()
 {
+  Serial.begin(9600);
+  HashType<char*,int> hashRawArray[HASH_SIZE];
+  HashMap<char*,int> hashMap = HashMap<char*,int>( hashRawArray , HASH_SIZE );
+
+  hashMap[0]("Value 1",2000);
+  
+  Serial.print(hashMap.getValueOf("Value 1"));
   //init();
   //StaticJsonBuffer<JSON_SIZE> jsonBuffer;
 //  root = jsonBuffer.createObject();
